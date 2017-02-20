de2f7e4315ddedb0de498e636b8a13e2a16b37c4
averybadtime <allenliu@wireless-10-105-81-112.umd.edu>
2017-02-20 16:26:24 -0500

    added methods to revert and convert from standard time to military time

--- src/MilitaryClock.java
+++ src/MilitaryClock.java
@@ -6,14 +6,71 @@ public class MilitaryClock extends Clock {
 
 	}
 	
+	public MilitaryClock(int hour, int minutes) {
+		super(hour, minutes);
+	}
+
 	public String convertedClock() {
 		int tempHour = hour;
+		String midnight = "0000";
+		String convertedHour;
+		String convertedMinute;
+		if (hour == 12 && minutes == 00 && timeOfDay.equals("AM")) {
+			return midnight;
+		}
 		if (timeOfDay.equals("AM")) {
-			return Integer.toString(hour) + Integer.toString(minutes);
+			if (hour < 10) {
+				convertedHour = "0" + Integer.toString(hour);
+			} else {
+				convertedHour = Integer.toString(hour);
+			}
+
+			if (minutes < 10) {
+				convertedMinute = "0" + Integer.toString(minutes);
+			} else {
+				convertedMinute = Integer.toString(minutes);
+			}
+
+			return convertedHour + convertedMinute;
+
 		} else {
 			tempHour += 12;
-			return Integer.toString(tempHour) + Integer.toString(minutes);
+			if (minutes < 10) {
+				convertedMinute = "0" + Integer.toString(minutes);
+			} else {
+				convertedMinute = Integer.toString(minutes);
+			}
+
+			return Integer.toString(tempHour) + convertedMinute;
 		}
+	}
+
+	public String revertClock() {
+		int tempHour = hour;
+		String convertedMinute;
+		String timeOfDay;
+
+		if (tempHour > 12) {
+			tempHour -= 12;
+			timeOfDay = "PM";
+			if (minutes < 10) {
+				convertedMinute = "0" + Integer.toString(minutes);
+			} else {
+				convertedMinute = Integer.toString(minutes);
+			}
+
+			return Integer.toString(tempHour) + ":" + convertedMinute + timeOfDay;
+		} else {
+			timeOfDay = "AM";
+			if (minutes < 10) {
+				convertedMinute = "0" + Integer.toString(minutes);
+			} else {
+				convertedMinute = Integer.toString(minutes);
+			}
+
+			return Integer.toString(tempHour) + ":" + convertedMinute + " " + timeOfDay;
+		}
+
 	}
 
 }
